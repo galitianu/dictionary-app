55a8321572c76d35f705e1f90ee5821be99c8115
Andrei Galitianu <andrei.galitianu@ciconia-systems.com>
2023-04-28 10:45:20 +0300

    last minute fix

--- src/service/favouritesService.js
+++ src/service/favouritesService.js
@@ -14,9 +14,9 @@ export function getCurrentDate(separator = "-") {
   let date = newDate.getDate();
   let month = newDate.getMonth() + 1;
   let year = newDate.getFullYear();
-  let hour = newDate.getHours().toString(2);
+  let hour = newDate.getHours().toString();
   let minute = newDate.getMinutes();
-
+  console.log(hour);
   return `${year}${separator}${
     month < 10 ? `0${month}` : `${month}`
   }${separator}${date}, ${hour < 10 ? `0${hour}` : `${hour}`}:${
